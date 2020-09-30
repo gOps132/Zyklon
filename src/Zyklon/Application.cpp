d1ce47cb9a3a9f774e432e7ca5a92d1fb816b110
gOps132 <epilred@icloud.com>
2020-09-30 18:13:02 +0800

    initial commit

--- src/Zyklon/Application.cpp
+++ src/Zyklon/Application.cpp
@@ -56,24 +56,27 @@ namespace Zyklon {
 
             layout(location = 0) in vec3 a_Position;
 
+            out vec3 v_Position;
+
             void main() 
             {
+                v_Position = a_Position;
                 gl_Position = vec4(a_Position, 1.0);
-
             }            
         )";
 
         std::string fragmentSrc = R"(
             #version 330 core
 
+            in vec3 v_Position;   
+
             layout(location = 0) out vec4 color;
 
             void main() 
             {
-                color = vec4(0.8, 0.2, 0.3, 1.0);
+                color = vec4(v_Position * 0.5 + 0.5, 1.0);
             }            
         )";
-
 
         m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
     }
