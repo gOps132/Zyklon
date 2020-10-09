3149ff05881769f0f12e07499bc8504ac00765c2
Gian Cedrick G. Epilan <epilred@icloud.com>
2020-10-09 16:03:40 +0800

    fixed spdlog segmentation fault bug

--- src/Zyklon/Log.cpp
+++ src/Zyklon/Log.cpp
@@ -7,16 +7,13 @@ namespace Zyklon
     std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
     std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
 
-// set pattern may cause segementation fault in multithreaded program
-// https://github.com/gabime/spdlog/issues/419
-// Aug 6 11:43 segmentation fault due to spd log, this init function may be the cause.
     void Log::init()
     {
         spdlog::set_pattern("%^[%T] %n: %v%$");
         s_CoreLogger = spdlog::stdout_color_mt("ZYKLON");
         s_CoreLogger->set_level(spdlog::level::trace);
 
-        s_CoreLogger = spdlog::stdout_color_mt("APP");
-        s_CoreLogger->set_level(spdlog::level::trace);
+        s_ClientLogger = spdlog::stdout_color_mt("APP");
+        s_ClientLogger->set_level(spdlog::level::trace);
     }
 }
