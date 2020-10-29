a221471bb769697c2c0adc4046dd023c8509b8cb
gOps132 <epilred@icloud.com>
2020-10-29 20:24:17 +0800

    fc added helper function to convert to opengl base type

--- src/Renderer/Buffer.h
+++ src/Renderer/Buffer.h
@@ -1,8 +1,10 @@
 #ifndef __BUFFER_H__
 #define __BUFFER_H__
 
-#include "Zyklon/Core.h"
 #include "Zyklon/zyklon_pch.h"
+#include "Zyklon/Core.h"
+
+#include <glad/glad.h>
 
 namespace Zyklon {
 
@@ -24,47 +26,80 @@ enum class ShaderDataType : uint32_t {
 
 static uint32_t ShaderDataTypeSize(ShaderDataType pType)
 {
-    switch (pType) {
-
-    case ShaderDataType::Float:
-        return 4;
-    case ShaderDataType::Float2:
-        return 4 * 2;
-    case ShaderDataType::Float3:
-        return 4 * 3;
-    case ShaderDataType::Float4:
-        return 4 * 4;
-    case ShaderDataType::Mat3:
-        return 4 * 3 * 3;
-    case ShaderDataType::Mat4:
-        return 4 * 4 * 4;
-        case ShaderDataType::Int:
-        return 4;
-    case ShaderDataType::Int2:
-        return 4 * 2;
-    case ShaderDataType::Int3:
-        return 4 * 3;
-    case ShaderDataType::Int4:
-        return 4 * 4;
-    case ShaderDataType::Bool:
-        return 1;
+    switch (pType) 
+    {
+        case ShaderDataType::Float:     return 4;
+        case ShaderDataType::Float2:    return 4 * 2;
+        case ShaderDataType::Float3:    return 4 * 3;
+        case ShaderDataType::Float4:    return 4 * 4;
+        case ShaderDataType::Mat3:      return 4 * 3 * 3;
+        case ShaderDataType::Mat4:      return 4 * 4 * 4;
+        case ShaderDataType::Int:       return 4;
+        case ShaderDataType::Int2:      return 4 * 2;
+        case ShaderDataType::Int3:      return 4 * 3;
+        case ShaderDataType::Int4:      return 4 * 4;
+        case ShaderDataType::Bool:      return 1;
     }
 
-    ZYKLON_CORE_ASSERT(false, "Unkown shader data type");
+    ZYKLON_CORE_ASSERT(false, "Unkown shader data Type");
+    return 0;
+}
+
+// temporary helper function
+static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+{
+    switch (type)
+    {
+
+        case ShaderDataType::Float:     return GL_FLOAT;
+        case ShaderDataType::Float2:    return GL_FLOAT;
+        case ShaderDataType::Float3:    return GL_FLOAT;
+        case ShaderDataType::Float4:    return GL_FLOAT;
+        case ShaderDataType::Mat3:      return GL_FLOAT;
+        case ShaderDataType::Mat4:      return GL_FLOAT;
+        case ShaderDataType::Int:       return GL_INT;
+        case ShaderDataType::Int2:      return GL_INT;
+        case ShaderDataType::Int3:      return GL_INT;
+        case ShaderDataType::Int4:      return GL_INT;
+        case ShaderDataType::Bool:      return GL_BOOL; 
+    } 
+    
+    ZYKLON_CORE_ASSERT(false, "Unkown Shader type");
     return 0;
 }
 
 struct BufferElement {
+    std::string Name;
+    ShaderDataType Type;
+    uint32_t Offset;
+    uint32_t Size;
+
     BufferElement(ShaderDataType pType, const std::string &pName)
-        : m_Name(pName), m_Type(ShaderDataTypeSize(pType)), m_Offset(0),
-          m_Size(0)
+        : Name(pName), Type(pType), Offset(0), Size(0)
     {
     }
 
-    std::string m_Name;
-    uint32_t m_Type;
-    uint32_t m_Offset;
-    uint32_t m_Size;
+    uint32_t GetComponentCount() const
+    {
+        switch (Type) 
+        {
+            case ShaderDataType::Float:     return 1;
+            case ShaderDataType::Float2:    return 2;
+            case ShaderDataType::Float3:    return 3;
+            case ShaderDataType::Float4:    return 4;
+            case ShaderDataType::Mat3:      return 3 * 3;
+            case ShaderDataType::Mat4:      return 4 * 4;
+            case ShaderDataType::Int:       return 1;
+            case ShaderDataType::Int2:      return 2;
+            case ShaderDataType::Int3:      return 3;
+            case ShaderDataType::Int4:      return 4;
+
+            case ShaderDataType::Bool:      return 1;
+        }
+
+        ZYKLON_CORE_ASSERT(false, "Unkown Shader type");
+        return 0;
+    }
 };
 
 class BufferLayout {
@@ -72,7 +107,7 @@ class BufferLayout {
     BufferLayout(const std::initializer_list<BufferElement> &element)
         : m_BufferElements(element)
     {
-        calculate_offsets_and_stride();
+        calculateOffsetsAndStrides();
     }
 
     inline const std::vector<BufferElement> &getBufferElements() const
@@ -80,15 +115,24 @@ class BufferLayout {
         return m_BufferElements;
     }
 
-    void calculate_offsets_and_stride()
+    void calculateOffsetsAndStrides()
     {
-        uint32_t offset = 0;
+        uint32_t l_offset = 0;
         m_Stride = 0;
         for (auto &element : m_BufferElements) {
-            element.m_Offset = offset;
-            offset += element.m_Size;
-            m_Stride += element.m_Size;
+            element.Offset = l_offset;
+            l_offset += element.Size;
+            m_Stride += element.Size;
         }
+    }
+
+    std::vector<BufferElement>::iterator begin()
+    {
+        return m_BufferElements.begin();
+    }
+    std::vector<BufferElement>::iterator end()
+    {
+        return m_BufferElements.end();
     }
 
   private:
@@ -102,6 +146,9 @@ class VertexBuffer {
 
     virtual void Bind() const = 0;
     virtual void Unbind() const = 0;
+
+    virtual void SetLayout(const BufferLayout &layout) = 0;
+    virtual const BufferLayout &GetLayout() const = 0;
 
     static VertexBuffer *Create(float *vertices, size_t size);
 };
