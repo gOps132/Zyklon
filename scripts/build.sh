977cc8a1810a72746543336d37d80f07a9377534
Gian Cedrick G. Epilan <epilred@icloud.com>
2020-11-03 18:17:19 +0800

    bc modified build scripts for better builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,13 +10,20 @@ if [ ! -d "build" ]; then
 fi
 cd build
 cmake \
-    -DCMAKE_CXX_COMPILER="g++" \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
     ..
 
-make -j $(nproc)
+# TODO: if os is linux
+    # make -j $(nproc)
+# TODO: if os is macOS
+    make -j $(sysctl -n hw.ncpu)
 
 cd ..
 
 ./bin/Sandbox-Debug/sandbox
+
+# There seems to be some issues with the g++-10 compiler with macOS, likely
+# because this kind architecture or kernel is not supported, I have to specify
+# a special executable from gcc for the darwin kernel "x86_64-apple-darwin18-g++-10"
+# apparently that does not work also.
