0d7d21a02f9411791777977f577d1943e103bc6f
Gian Cedrick G. Epilan <epilred@icloud.com>
2020-11-28 18:45:47 +0800

    fixed renderer api

--- src/Renderer/Renderer.cpp
+++ src/Renderer/Renderer.cpp
@@ -11,6 +11,13 @@
 
 namespace Zyklon {
 
+RendererAPI Renderer::s_RendererAPI = RendererAPI::OpenGL;
+
+void Renderer::set_renderer_api(RendererAPI p_renderer_api) 
+{
+    Renderer::s_RendererAPI = p_renderer_api;        
+}
+
 GraphicsContext *GraphicsContext::create(GLFWwindow *p_window)
 {
     switch (Renderer::get_api()) {
