0d7b133afad2520c9f93f25d6af883c420663e4b
Gian <epilred@icloud.com>
2024-11-20 16:46:09 +0800

    fixed flipping issue

--- src/Renderer/Camera/OrbitControls.cpp
+++ src/Renderer/Camera/OrbitControls.cpp
@@ -21,34 +21,17 @@ namespace Zyklon
 		glm::mat4 pitch_rotation = glm::rotate(glm::mat4(1.0f), m_pitch, glm::vec3(1.0f, 0.0f, 0.0f));
 
 		glm::vec3 direction = glm::vec3(yaw_rotation * pitch_rotation * glm::vec4(0,0,-1,1));
+		// Calculate the "right" vector from direction
+		glm::vec3 right = glm::normalize(glm::cross(direction, glm::vec3(0, 1, 0)));
 
-		// // calculate distance from the camera to the target
-		// m_distance = glm::length(m_target - m_camera->get_position());
+		// Dynamically calculate the "up" vector
+		glm::vec3 up = glm::normalize(glm::cross(right, direction));
 
 		// update the camera position 
 		glm::vec3 camera_pos = m_target + direction * m_distance;
 
 		// update camera values
-		m_camera->look_at(camera_pos, m_target, {0.0f, 1.0f, 0.0f});
+		m_camera->look_at(camera_pos, m_target, up);
 	}
-
-/*
-	glm::vec3 map_to_unit_sphere(float p_mouse_x, float p_mouse_y, float p_width, float p_height)
-	{
-		glm::vec3 v;
-		v.x = (2.0f * p_mouse_x - p_width) / p_width;
-		v.y = (p_height - 2.0f * p_mouse_y) / p_height;
-		v.z = 0.0f;
-
-		float length = v.x * v.x + v.y * v.y;
-
-		if (length > 1.0f)
-			v =  glm::normalize(v);
-		else
-			v.z = sqrt(1.0f - length);
-
-		return v;
-	}
-*/
 
 } // namespace Zyklon
--- src/Renderer/Camera/OrbitControls.h
+++ src/Renderer/Camera/OrbitControls.h
@@ -21,17 +21,13 @@ namespace Zyklon
 		void set_target(glm::vec3 p_target) { m_target = p_target; }
 		void set_camera(std::shared_ptr<Camera> p_camera) { m_camera = p_camera; }
 		void set_rotation_speed(float rotation_speed) { m_rotation_speed = rotation_speed; }
-/*
-		// convert the 2d screen coordinates into 3d movement
-		glm::vec3 map_to_unit_sphere(float p_mouse_x, float p_mouse_y, float p_width, float p_height);
-*/
 		void update(float dx = 0.0f, float dy = 0.0f);
 	private:
 		std::shared_ptr<Camera> m_camera;
 		glm::vec3 m_target;
 		float m_yaw = 0.0f;
 		float m_pitch = 0.0f;
-		float m_rotation_speed = 0.1f;
+		float m_rotation_speed = 0.05f;
 		float m_distance;
 	};
 }
